d98a5bc454130f189fe12132553f7b2992e7b723
NgocAnh <ngocanh411997@gmail.com>
2018-05-29 21:31:43 +0700

    [Ánh]

--- insert.sql
+++ insert.sql
@@ -98,3 +98,103 @@ VALUES  ( 'HS05' , -- MaHS - varchar(10)
           N'Không' , -- TonGiao - nvarchar(50)
           'ML02'  -- MaLop - varchar(10)
         )
+GO
+INSERT INTO dbo.HocSinh
+        ( MaHS ,
+          TenHS ,
+          GioiTinh ,
+          NgaySinh ,
+          DiaChi ,
+          DanToc ,
+          TonGiao ,
+          MaLop
+        )
+VALUES  ( 'HS06' , -- MaHS - varchar(10)
+          N'Nguyễn Văn Huy' , -- TenHS - nvarchar(50)
+          N'Nam' , -- GioiTinh - nvarchar(5)
+          '2001-04-19' , -- NgaySinh - date
+          N'Hà Nội' , -- DiaChi - nvarchar(150)
+          N'Kinh' , -- DanToc - nvarchar(50)
+          N'Không' , -- TonGiao - nvarchar(50)
+          'ML02'  -- MaLop - varchar(10)
+        )
+GO
+INSERT INTO dbo.HocSinh
+        ( MaHS ,
+          TenHS ,
+          GioiTinh ,
+          NgaySinh ,
+          DiaChi ,
+          DanToc ,
+          TonGiao ,
+          MaLop
+        )
+VALUES  ( 'HS07' , -- MaHS - varchar(10)
+          N'Nguyễn Mạnh Nam' , -- TenHS - nvarchar(50)
+          N'Nam' , -- GioiTinh - nvarchar(5)
+          '2001-02-05' , -- NgaySinh - date
+          N'Hà Nội' , -- DiaChi - nvarchar(150)
+          N'Kinh' , -- DanToc - nvarchar(50)
+          N'Không' , -- TonGiao - nvarchar(50)
+          'ML02'  -- MaLop - varchar(10)
+        )
+GO
+INSERT INTO dbo.HocSinh
+        ( MaHS ,
+          TenHS ,
+          GioiTinh ,
+          NgaySinh ,
+          DiaChi ,
+          DanToc ,
+          TonGiao ,
+          MaLop
+        )
+VALUES  ( 'HS08' , -- MaHS - varchar(10)
+          N'Nguyễn Bá Quang' , -- TenHS - nvarchar(50)
+          N'Nam' , -- GioiTinh - nvarchar(5)
+          '2002-02-05' , -- NgaySinh - date
+          N'Hà Nội' , -- DiaChi - nvarchar(150)
+          N'Kinh' , -- DanToc - nvarchar(50)
+          N'Không' , -- TonGiao - nvarchar(50)
+          'ML03'  -- MaLop - varchar(10)
+        )
+GO
+INSERT INTO dbo.HocSinh
+        ( MaHS ,
+          TenHS ,
+          GioiTinh ,
+          NgaySinh ,
+          DiaChi ,
+          DanToc ,
+          TonGiao ,
+          MaLop
+        )
+VALUES  ( 'HS09' , -- MaHS - varchar(10)
+          N'Nguyễn Văn Hạnh' , -- TenHS - nvarchar(50)
+          N'Nam' , -- GioiTinh - nvarchar(5)
+          '2002-05-05' , -- NgaySinh - date
+          N'Hà Nội' , -- DiaChi - nvarchar(150)
+          N'Kinh' , -- DanToc - nvarchar(50)
+          N'Không' , -- TonGiao - nvarchar(50)
+          'ML03'  -- MaLop - varchar(10)
+        )
+GO
+INSERT INTO dbo.HocSinh
+        ( MaHS ,
+          TenHS ,
+          GioiTinh ,
+          NgaySinh ,
+          DiaChi ,
+          DanToc ,
+          TonGiao ,
+          MaLop
+        )
+VALUES  ( 'HS10' , -- MaHS - varchar(10)
+          N'Đào Nguyễn Nam' , -- TenHS - nvarchar(50)
+          N'Nam' , -- GioiTinh - nvarchar(5)
+          '2002-01-15' , -- NgaySinh - date
+          N'Hà Nội' , -- DiaChi - nvarchar(150)
+          N'Kinh' , -- DanToc - nvarchar(50)
+          N'Không' , -- TonGiao - nvarchar(50)
+          'ML03'  -- MaLop - varchar(10)
+        )
