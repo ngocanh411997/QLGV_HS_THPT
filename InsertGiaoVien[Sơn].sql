0f53754b1306835fb0c8fe9a7a6bb7ce6b4fec36
HongSonMTA <36963575+HongSonMTA@users.noreply.github.com>
2018-06-10 10:27:19 +0700

    [Sơn] Insert Dữ Liệu Giáo Viên

---
+++ InsertGiaoVien[Sơn].sql
@@ -0,0 +1,32 @@
+﻿USE QLGV_HS_THPT
+GO
+INSERT INTO dbo.GiaoVien
+        ( MaGV ,TenGV ,GioiTinh ,NgaySinh , SDT , DiaChi , Luong ,MaMon)
+VALUES  ( 'GV01' , N'Nguyễn Hồng Sơn' ,  N'Nam' ,'12/04/1980', '01111111111' ,  N'Hà Nội' , 10000000 , 'MH01')
+INSERT INTO dbo.GiaoVien
+        ( MaGV ,TenGV ,GioiTinh ,NgaySinh , SDT , DiaChi , Luong ,MaMon)
+VALUES  ( 'GV02' , N'Vương Văn Thanh' ,  N'Nam' ,'12/12/1975', '0123232323' ,  N'Hà Nội' , 1100000 , 'MH03')
+INSERT INTO dbo.GiaoVien
+        ( MaGV ,TenGV ,GioiTinh ,NgaySinh , SDT , DiaChi , Luong ,MaMon)
+VALUES  ( 'GV03' , N'Nguyễn Văn Tính' ,  N'Nam' ,'26/04/1985', '01434343434' ,  N'Hà Nội' , 7000000 , 'MH04')
+INSERT INTO dbo.GiaoVien
+        ( MaGV ,TenGV ,GioiTinh ,NgaySinh , SDT , DiaChi , Luong ,MaMon)
+VALUES  ( 'GV04' , N'Nguyễn Văn Bách' ,  N'Nam' ,'15/08/1980', '014545454545' ,  N'Hà Nội' ,8000000 , 'MH05')
+INSERT INTO dbo.GiaoVien
+        ( MaGV ,TenGV ,GioiTinh ,NgaySinh , SDT , DiaChi , Luong ,MaMon)
+VALUES  ( 'GV05' , N'Phạm Quang Minh' ,  N'Nam' ,'24/04/1989', '023445566544' ,  N'Hà Nội' , 15000000 , 'MH02')
+INSERT INTO dbo.GiaoVien
+        ( MaGV ,TenGV ,GioiTinh ,NgaySinh , SDT , DiaChi , Luong ,MaMon)
+VALUES  ( 'GV06' , N'Nguyễn Thị Ngọc Ánh' ,  N'Nữ' ,'15/01/1990', '089898989' ,  N'Hà Nội' , 7000000 , 'MH06')
+INSERT INTO dbo.GiaoVien
+        ( MaGV ,TenGV ,GioiTinh ,NgaySinh , SDT , DiaChi , Luong ,MaMon)
+VALUES  ( 'GV07' , N'Phạm Hồng Sơn' ,  N'Nam' ,'12/06/1980', '0164746373' ,  N'Hà Nội' , 9000000 , 'MH07')
+INSERT INTO dbo.GiaoVien
+        ( MaGV ,TenGV ,GioiTinh ,NgaySinh , SDT , DiaChi , Luong ,MaMon)
+VALUES  ( 'GV08' , N'Phan Duy Dương ' ,  N'Nam' ,'30/06/1986', '0164746373' ,  N'Hà Nội' , 9000000 , 'MH01')
+INSERT INTO dbo.GiaoVien
+        ( MaGV ,TenGV ,GioiTinh ,NgaySinh , SDT , DiaChi , Luong ,MaMon)
+VALUES  ( 'GV09' , N'Ngô Mạnh Quyền' ,  N'Nam' ,'26/08/1984', '0164746373' ,  N'Hà Nội' , 9000000 , 'MH02')
+INSERT INTO dbo.GiaoVien
+        ( MaGV ,TenGV ,GioiTinh ,NgaySinh , SDT , DiaChi , Luong ,MaMon)
+VALUES  ( 'GV10' , N'Nguyễn Văn Bắc' ,  N'Nam' ,'26/09/1984', '0164746373' ,  N'Hà Nội' , 9000000 , 'MH02')
